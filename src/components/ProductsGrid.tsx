615d3ffd49a86b961af76c7351690aa222ebf8a7
Deepayan Ghosh <deepayan.dg@gmail.com>
2025-12-07 18:03:08 +0530

    Map SingleProductCard components from each array items of Product object

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -1,13 +1,62 @@
+import { useState } from "react";
 import SingleProductCard from "./SingleProductCard";
 
+const productDefaultImg = '/react-display-1.webp';
+
+export interface Product {
+    id: number,
+    name: string,
+    category: string,
+    image: string,
+    price: number,
+    off_percent: number,
+    rating: number,
+    reviews: number
+}
+
+const allProducts: Product[] = [
+    {
+        id: 1,
+        name: "Product 1",
+        category: "Category 1",
+        image: productDefaultImg,
+        price: 100,
+        off_percent: 0, 
+        rating: 4,
+        reviews: 10
+    },
+    {
+        id: 2,
+        name: "Product 2",
+        category: "Category 2",
+        image: productDefaultImg,
+        price: 200,
+        off_percent: 0, 
+        rating: 4,
+        reviews: 10
+    },
+    {
+        id: 3,
+        name: "Product 3",
+        category: "Category 1",
+        image: productDefaultImg,
+        price: 500,
+        off_percent: 0, 
+        rating: 4,
+        reviews: 10
+    }
+]
+
 export default function ProductsGrid() {
+
+    const [ products, setProducts ] = useState<Product[]>( allProducts )
+
     return (
         <div className="grid w-full grid-cols-2 gap-6">
-            <SingleProductCard />
-            <SingleProductCard />
-            <SingleProductCard />
-            <SingleProductCard />
-            <SingleProductCard />
+            {
+                /* singleProduct prop = product object (interface Product) */
+                products.map( ( product ) => <SingleProductCard key={product.id} singleProduct = {product} /> )
+            }
         </div>
     )
 }
--- src/components/SingleProductCard.tsx
+++ src/components/SingleProductCard.tsx
@@ -1,3 +1,44 @@
-export default function SingleProductCard() {
-    return <></>
+import { Heart, ShoppingCart } from "lucide-react";
+import { ProductRating } from "./ProductRating";
+import type { Product } from "./ProductsGrid";
+
+export default function SingleProductCard( {singleProduct}: {singleProduct: Product} ) {
+    return (
+        <div className="h-[360px] bg-white rounded-2xl shadow-lg overflow-hidden hover:shadow-xl transition-shadow duration-300">
+            <div className="relative">
+                <img
+                src="/react-display-1.webp"
+                alt="SilkSculpt Serum"
+                className=""
+                />
+                <DiscountBadge discount="50% off" />
+                <button className="absolute top-4 right-4 bg-white/80 backdrop-blur-sm p-2 rounded-full hover:bg-white transition-colors z-10">
+                    <Heart className="w-5 h-5 text-gray-600" />
+                </button>
+            </div>
+
+            <div className="p-5 space-y-3">
+                <div className="flex justify-between">
+                    <p className="text-sm text-gray-500">{singleProduct.category}</p>
+                    <ProductRating rating={4.9} reviews={128} />
+                </div>
+                <h3 className="text-xl font-semibold text-gray-900">{singleProduct.name}</h3>
+                <div className="flex items-center gap-3">
+                <span className="text-xl font-bold text-gray-900">$35.00</span>
+                <span className="text-md text-gray-400 line-through">$70.00</span>
+                <button className="ml-auto flex items-center gap-2 bg-emerald-600 text-white px-4 py-2 rounded-full font-medium hover:bg-emerald-700 transition-colors">
+                    <ShoppingCart className="w-5 h-5" /> Add
+                </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function DiscountBadge({ discount }: { discount: string }) {
+    return (
+        <div className="absolute top-4 left-4 bg-emerald-600 text-white text-sm font-semibold px-3 py-1.5 rounded-full z-10">
+            {discount}
+        </div>
+    );
 }
